e75f37b7636cb5a44dc9be7b510bd2bd503a0aa6
sonamdivyanshi1 <sonam.divyanshi@wisdmlabs.com>
2023-03-28 23:06:28 +0530

    separated footer part

---
+++ footer.php
@@ -0,0 +1,67 @@
+<footer>
+        <div style="width:15vw; margin-top: 10px;">
+            <!-- <a class="logo" href="#"></a><i class="fa-brands fa-instalod fa-2xl"></i></a> -->
+            <img src="<?php echo get_template_directory_uri(); ?> /assests/images/Logo.png" height="48" width="48">
+            <p id="footer-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+                dummy text ever since the 1500s</p>
+            <ul class="horizontal-list">
+                <li>
+                    <a href="#">
+                        <i class="fa-brands fa-facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </li>
+            </ul> 
+        </div>
+        <div>
+            <h3>Product</h3>
+            <p>Connections</p>
+            <p>Protocols</p>
+            <p>Personas</p>
+            <p>Integration</p>
+            <p>Catalog</p>
+            <p>Pricing</p>
+            <p>Security</p>
+            <p>GDPR</p>
+        </div>
+        <div>
+            <h3>For Developers</h3>
+            <p>Docs</p>
+            <p>API</p>
+            <p>Open Source</p>
+            <p>Engineering team</p>
+           
+        </div>
+        <div>
+            <h3>Company</h3>
+            <p>Careers</p>
+            <p>Blog</p>
+            <p>Press</p>
+        </div>
+        <div>
+            <h3>Support</h3>
+            <p>Help Center</p>
+            <p>Contact Us</p>
+            <p>Security</p>
+            <p>Bulletins</p>
+            <p>Documentation</p>
+            <p>Partner</p>
+            <p>Portal</p>
+        </div>
+        <div>
+            <h3>Newsletter</h3>
+            <input type="email" placeholder="Email" id="input-footer">
+        </div>
+        
+    </footer>
+    <p style="font-weight: 900; font-size: 16px; line-height: 16px; color: #160637; text-align: center;">Sonam Divyanshi  &#169 </p>
+</body>
