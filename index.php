ad3fc386423df1b3106d6859988bb66e36d51cc8
sonamdivyanshi1 <sonam.divyanshi@wisdmlabs.com>
2023-03-29 15:17:30 +0530

    html code added to home.php

--- index.php
+++ index.php
@@ -1,114 +1,15 @@
 <?php get_header(); ?>
 
-        <div id="main">
-            <div id="content">
-                <p id="tag">
-                    <i class="fa-solid fa-stars"></i>&nbsp;v3.1 released. 
-                    <a href="" style="color: #722ED1; text-decoration: underline;">Learn More</a>
-                </p>
-                <h1>Your data with real time analytics</h1>
-                <p>Harness the potential of big data analytics & Cloud services and become a data-driven organization with needle tail</p>
-                <button id="button-two">Start free trial</button>
-                <button id="button-three">Learn More</button>
-            </div>
-            <div><img src="<?php echo get_template_directory_uri(); ?> /assests/images/image 1.png" class="mobile-view"></div>
-            <!-- <div class="image"></div> -->
-        </div>
-    </div> 
-    <section id="qulby">
-        <button class="heading"><p class="topic"><i class="fa-solid fa-eye fa-l"></i>&nbsp; &nbsp;Why Qulby</p></button>
-        <h1 class="title">Get actionable insights from Big data in 3 steps</h1>
-        <p id="para">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
-            dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-        <div class="container">
-            <div class="box">
-                <!-- <i class="fa-solid fa-stairs"></i> -->
-                <img src="<?php echo get_template_directory_uri(); ?> /assests/images/chart-bar-33.png" height="40" width="40">
-                <h3 class="feature">Valuable business insights</h3>
-                <p class="text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                   </p>
-            </div>
-            <div class="box">
-                <img src="<?php echo get_template_directory_uri(); ?> /assests/images/newsletter-dev.png" height="40" width="40">
-                <!-- <i class="fa-solid fa-stairs"></i> -->
-                <h3  class="feature">Powerful algorithms</h3>
-                <p class="text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                    </p>
-            </div>
-            <div class="box">
-                <img src="<?php echo get_template_directory_uri(); ?> /assests/images/timeline.png" height="40" width="40">
-                <!-- <i class="fa-solid fa-stairs"></i> -->
-                <h3 class="feature">Data in real time</h3>
-                <p class="text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                    </p>
-            </div>
-        </div>
-    </section>
-    <section id="products">
-        <div class="inner">
-            <button class="heading"><p class="topic">
-                <!-- <i class="fa-solid fa-code-branch"></i> -->
-                <img src="<?php echo get_template_directory_uri(); ?> /assests/images/electronic-circuit.png" height="20" width="20">
-                &nbsp; &nbsp;For Products Team</p>
-            </button>
-            <p class="title">Launch with best stack</p>
-            <p class="text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
-        </div>
-        <div>
-            <img src="<?php echo get_template_directory_uri(); ?> /assests/images/Group 21.png" height="412" width="412" class="image mobile-view">
-        </div>
-    </section>
-    <section id="engineering">
-        <div class="inner">
-            <button class="heading"><p class="topic"><i class="fa-solid fa-code-branch"></i>&nbsp;&nbsp;For Engineering Teams</p></button>
-            <p class="title">Data-driven pipelines in minutes</p>
-            <p class="text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
-        </div>
-        <div >
-            <img src="<?php echo get_template_directory_uri(); ?> /assests/images/image 6.png" height="412" width="600" class="image mobile-view">
-        </div>
-    </section>
-    <section id="testinomial">
-        <p id="quote">
-            "What I love about Qulby is the easy way we can collaborate even if there is a lot of people involved in the process"
-        </p>
-        <img src="<?php echo get_template_directory_uri(); ?> /assests/images/Profile.png"id="profile">
-        <!-- <div id="profile"><i class="fa-solid fa-user fa-2xl"></i></div> -->
-        <p class="name">Guillame Cabane</p>
-        <p class="name" style="margin-bottom: 5vh;">CTO @ BigSpring</p>
-        <hr>
-        <div id="client-logos">
-                <a class="logos" href=""><img src="https://dr2hgwlj5hpvq.cloudfront.net/3/108580/48fa1efc-ce03-4d3b-a64e-0b0c8497c63f.png" width="145" height="40"></a>
-                <a class="logos" href=""><img src="https://dr2hgwlj5hpvq.cloudfront.net/3/108580/48fa1efc-ce03-4d3b-a64e-0b0c8497c63f.png" width="145" height="40"></a>
-                <a class="logos" href=""><img src="https://dr2hgwlj5hpvq.cloudfront.net/3/108580/48fa1efc-ce03-4d3b-a64e-0b0c8497c63f.png" width="145" height="40"></a>
-                <a class="logos" href=""><img src="https://dr2hgwlj5hpvq.cloudfront.net/3/108580/48fa1efc-ce03-4d3b-a64e-0b0c8497c63f.png" width="145" height="40"></a>
-                <a class="logos" href=""><img src="https://dr2hgwlj5hpvq.cloudfront.net/3/108580/48fa1efc-ce03-4d3b-a64e-0b0c8497c63f.png" width="145" height="40"></a>
-        </div>
-    </section>
-    <section id="signup">
-        <div id="trust">
-            <p id="trust-title">
-                See why the world's best companies use qulby to become truly data driven.
-            </p>
-            <div>
-                <p id="trusties">------------------Trusted by------------------</p>
-                
-                <img class="images" src="https://1000logos.net/wp-content/uploads/2021/04/Accenture-logo.png" height="100" width="150">
-                <img class="images" src="https://1000logos.net/wp-content/uploads/2021/04/Accenture-logo.png" height="100" width="150">
-                <img class="images" src="https://1000logos.net/wp-content/uploads/2021/04/Accenture-logo.png" height="100" width="150">
-                <img class="images" src="https://1000logos.net/wp-content/uploads/2021/04/Accenture-logo.png" height="100" width="150">
-            </div>
-        </div>
-        <div id="form">
-            <h2> Sign Up </h2>
-            <button id="button-four">Start your free trail</button>
-            <input type="email" placeholder="Your primary email">
-            <input type="password" placeholder="PassWord">
-            <input type="password" placeholder="Re-type password">
-            <h3>OR</h3>
-            <img src="https://onymos.com/wp-content/uploads/2020/10/google-signin-button-1024x260.png" width="200" height="48">
-            <!-- <hr> -->
-            <p>Already have an account?<a href="" style="color:#722ED1">Login</a></p>
-        </div>
-    </section>
-<?php get_footer(); ?>
+<?php
+    if ( have_posts() ) :
+        while ( have_posts() ) :
+            the_post();
+            the_title();
+            the_content();
+ 
+        endwhile;
+    else :
+        echo 'Sorry, no posts were found.';
+    endif;
+?>
+
